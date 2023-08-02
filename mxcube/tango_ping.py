67d11e54c450d38f9086d599b42bc4bd6852bb13
Elmir Jagudin <elmir.jagudin@maxiv.lu.se>
2023-08-02 18:11:11 +0200

    wait for sardana tango ds
    Wait until sardana tango device is pingable, before
    starting mxcube.
    
    MxCube needs the device running on start-up.

--- mxcube/tango_ping.py
+++ mxcube/tango_ping.py
@@ -1,9 +1,11 @@
 import time
-from tango import Database
-from PyTango import ConnectionFailed
+from tango import Database, DeviceProxy
+from PyTango import ConnectionFailed, DevFailed
 
 
 TANGO_CONNECT_RETRY_TIME = 0.6
+
+SARDANA_DS = "dserver/sardana/micromax"
 
 
 def _connect_to_db():
@@ -18,6 +20,19 @@ def _connect_to_db():
             time.sleep(TANGO_CONNECT_RETRY_TIME)
 
 
+def _ping_sardana_ds():
+    while True:
+        try:
+            DeviceProxy(SARDANA_DS).ping()
+            return
+        except DevFailed:
+            print(
+                f"failed to connect to {SARDANA_DS}, retrying in {TANGO_CONNECT_RETRY_TIME} seconds"
+            )
+            time.sleep(TANGO_CONNECT_RETRY_TIME)
+
+
 def wait_for_tango(*_, **__):
     _connect_to_db()
+    _ping_sardana_ds()
     return True

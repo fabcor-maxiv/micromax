41c144e55b77b2602539e9d71a94ea84d8904e54
Elmir Jagudin <elmir.jagudin@maxiv.lu.se>
2023-08-03 15:28:00 +0200

    use 'faux' motor for 'mono_energy'
    Use, custom written, FauxMotorController for emulating
    sardana 'mono_energy' moveable.
    
    The 'faux' moveables support 'StartPosition' property, thus
    we can start sardna with some other position then 0.0 for
    mono energy motor.
    
    Having mono energy at 0 breaks MxCube (division by zero).

---
+++ micromax-ds/faux.py
@@ -0,0 +1,84 @@
+from typing import Optional, Iterator
+from dataclasses import dataclass
+from sardana import State
+from sardana.pool.controller import MotorController
+from sardana.pool.controller import Type, Description
+
+
+@dataclass
+class _Motor:
+    start_position: Optional[float] = None
+    steps: Optional[Iterator[float]] = None
+    position: Optional[float] = None
+
+
+class FauxMotorController(MotorController):
+    axis_attributes = {
+        "StartPosition": {
+            Type: float,
+            Description: "moveable will start at this position, when Pool is initialized",
+        },
+    }
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self._motors = {}
+
+    def getStartPosition(self, axis):
+        # print(f"getStartPosition {axis=}")
+        return self._motors[axis].start_position
+
+    def setStartPosition(self, axis, value):
+        # print(f"setStartPosition {axis=} {value=}")
+        motor = self._motors[axis]
+
+        motor.start_position = value
+
+        if motor.position is None:
+            motor.position = value
+
+    def AddDevice(self, axis):
+        # print(f"AddDevice {axis=}")
+        self._motors[axis] = _Motor()
+
+    def DeleteDevice(self, axis):
+        # print(f"DeleteDevice {axis=}")
+        del self._motor[axis]
+
+    def StateOne(self, axis):
+        # print(f"StateOne {axis=}")
+        if self._motors[axis].steps:
+            return State.Moving
+
+        return State.On
+
+    def _move_motor(self, motor: _Motor):
+        if motor.steps is None:
+            # no move steps, nothing to do here
+            return
+
+        try:
+            motor.position = next(motor.steps)
+        except StopIteration:
+            motor.steps = None
+
+        motor.position = motor.target_position
+
+    def ReadOne(self, axis):
+        # print(f"ReadOne {axis=}")
+        motor = self._motors[axis]
+        self._move_motor(motor)
+
+        return motor.position
+
+    def StartOne(self, axis, position):
+        # print(f"StartOne {axis=} {position=}")
+        def steps(start, end, step_nums):
+            step = (end - start) / step_nums
+            for n in range(step_nums):
+                yield start + step * n
+
+            yield end
+
+        motor = self._motors[axis]
+        motor.steps = steps(motor.position, position, 10)
--- mysql/z_micromax_tango.sql
+++ mysql/z_micromax_tango.sql
@@ -10,9 +10,14 @@ VALUES ('Micromax/Door/01','Door_Micromax','Micromax','Door','01',0,'nada','nada
        ('dserver/Starter/aff41398c13a',NULL,'dserver','Starter','aff41398c13a',0,'nada','nada','Starter/aff41398c13a',0,'DServer','0',NULL,NULL,NULL),
        ('controller/basicdummymotorcontroller/dummy_ctrl','dummy_ctrl','controller','basicdummymotorcontroller','dummy_ctrl',1,'IOR:010000001700000049444c3a54616e676f2f4465766963655f353a312e3000000100000000000000ac000000010102000b0000003137322e32312e302e380000c98a00000e000000fe2ba99d6400000018000000000400000300000000000000080000000100000000545441010000001c000000010000000100010001000000010001050901010001000000090101000254544140000000010000000d00000062663761393336363163656600000000240000002f746d702f6f6d6e692d726f6f742f3030303030303032342d3136383830353430353900','bf7a93661cef','Sardana/micromax',24,'Controller','5','2023-06-29 15:57:08',NULL,NULL),
        ('motor/dummy_ctrl/1','tabd_zi','motor','dummy_ctrl','1',1,'IOR:010000001700000049444c3a54616e676f2f4465766963655f353a312e3000000100000000000000ac000000010102000b0000003137322e32312e302e380000c98a00000e000000fe2ba99d6400000018000000000500000300000000000000080000000100000000545441010000001c000000010000000100010001000000010001050901010001000000090101000254544140000000010000000d00000062663761393336363163656600000000240000002f746d702f6f6d6e692d726f6f742f3030303030303032342d3136383830353430353900','bf7a93661cef','Sardana/micromax',24,'Motor','5','2023-06-29 15:57:22',NULL,NULL),
-       ('motor/dummy_ctrl/2','mono_energy','motor','dummy_ctrl','2',1,'IOR:010000001700000049444c3a54616e676f2f4465766963655f353a312e3000000100000000000000ac000000010102000c0000003137322e32312e302e313000b9e800000e000000fe466dca6400000019000000000600000300000000000000080000000100000000545441010000001c000000010000000100010001000000010001050901010001000000090101000254544140000000010000000d00000034313237363562373130333600000000240000002f746d702f6f6d6e692d726f6f742f3030303030303032352d3136393039383738343600','412765b71036','Sardana/micromax',25,'Motor','5','2023-08-02 14:54:31',NULL,NULL),
        ('B312a-EH/DIA/DET-01',NULL,'B312a-EH','DIA','DET-01',0,'nada','nada','Eiger/Micromax',0,'Eiger','0',NULL,NULL,NULL),
-       ('dserver/Eiger/Micromax',NULL,'dserver','Eiger','Micromax',0,'nada','nada','Eiger/Micromax',0,'DServer','0',NULL,NULL,NULL);
+       ('dserver/Eiger/Micromax',NULL,'dserver','Eiger','Micromax',0,'nada','nada','Eiger/Micromax',0,'DServer','0',NULL,NULL,NULL),
+       ('controller/fauxmotorcontroller/faux_ctrl','faux_ctrl','controller','fauxmotorcontroller','faux_ctrl',1,'IOR:010000001700000049444c3a54616e676f2f4465766963655f353a312e3000000100000000000000ac000000010102000c0000003137322e32312e302e313000cbd000000e000000fe66a4cb6400000019000000000600000300000000000000080000000100000000545441010000001c000000010000000100010001000000010001050901010001000000090101000254544140000000010000000d00000062613032386562373033313800000000240000002f746d702f6f6d6e692d726f6f742f3030303030303032352d3136393130363734393400','ba028eb70318','Sardana/micromax',25,'Controller','5','2023-08-03 12:59:33',NULL,NULL),
+       ('motor/faux_ctrl/1','mono_energy','motor','faux_ctrl','1',1,'IOR:010000001700000049444c3a54616e676f2f4465766963655f353a312e3000000100000000000000ac000000010102000c0000003137322e32312e302e313000cbd000000e000000fe66a4cb6400000019000000000700000300000000000000080000000100000000545441010000001c000000010000000100010001000000010001050901010001000000090101000254544140000000010000000d00000062613032386562373033313800000000240000002f746d702f6f6d6e692d726f6f742f3030303030303032352d3136393130363734393400','ba028eb70318','Sardana/micromax',25,'Motor','5','2023-08-03 12:59:49',NULL,NULL);
+
+
+INSERT INTO `property_attribute_device`
+VALUES ('motor/faux_ctrl/1','StartPosition','__value',1,'5277.64','2023-08-03 13:01:11','2023-08-03 13:01:11',NULL);
 
 
 INSERT INTO `property_device`
@@ -34,29 +39,12 @@ VALUES ('MacroServer/micromax/1','PoolNames','','','',1,'Pool_micromax_1','2023-
        ('motor/dummy_ctrl/1','id','','','',1,'2','2023-06-29 15:57:22','2023-06-29 15:57:22',NULL),
        ('motor/dummy_ctrl/1','ctrl_id','','','',1,'1','2023-06-29 15:57:22','2023-06-29 15:57:22',NULL),
        ('motor/dummy_ctrl/1','axis','','','',1,'1','2023-06-29 15:57:22','2023-06-29 15:57:22',NULL),
-       ('motor/dummy_ctrl/2','id','','','',1,'3','2023-08-02 14:54:31','2023-08-02 14:54:31',NULL),
-       ('motor/dummy_ctrl/2','ctrl_id','','','',1,'1','2023-08-02 14:54:31','2023-08-02 14:54:31',NULL),
-       ('motor/dummy_ctrl/2','axis','','','',1,'2','2023-08-02 14:54:31','2023-08-02 14:54:31',NULL);
+       ('pool/micromax/1','PoolPath','','','',1,'/sardana_ctrls','2023-08-03 12:51:06','2023-08-03 12:51:06',NULL),
+       ('controller/fauxmotorcontroller/faux_ctrl','type','','','',1,'Motor','2023-08-03 12:59:33','2023-08-03 12:59:33',NULL),
+       ('controller/fauxmotorcontroller/faux_ctrl','library','','','',1,'faux.py','2023-08-03 12:59:33','2023-08-03 12:59:33',NULL),
+       ('controller/fauxmotorcontroller/faux_ctrl','klass','','','',1,'FauxMotorController','2023-08-03 12:59:33','2023-08-03 12:59:33',NULL),
+       ('controller/fauxmotorcontroller/faux_ctrl','id','','','',1,'3','2023-08-03 12:59:33','2023-08-03 12:59:33',NULL),
+       ('motor/faux_ctrl/1','id','','','',1,'4','2023-08-03 12:59:49','2023-08-03 12:59:49',NULL),
+       ('motor/faux_ctrl/1','ctrl_id','','','',1,'3','2023-08-03 12:59:49','2023-08-03 12:59:49',NULL),
+       ('motor/faux_ctrl/1','axis','','','',1,'1','2023-08-03 12:59:49','2023-08-03 12:59:49',NULL);
 
-
-
-
-INSERT INTO `property_device_hist`
-VALUES (3,'2023-06-29 09:05:13','MacroServer/micromax/1','PoolNames',1,'Pool_micromax_1'),
-       (4,'2023-06-29 09:05:13','tango/admin/aff41398c13a','polled_attr',1,'hoststate'),
-       (4,'2023-06-29 09:05:13','tango/admin/aff41398c13a','polled_attr',2,'1000'),
-       (4,'2023-06-29 09:05:13','tango/admin/aff41398c13a','polled_attr',3,'runningservers'),
-       (4,'2023-06-29 09:05:13','tango/admin/aff41398c13a','polled_attr',4,'1000'),
-       (4,'2023-06-29 09:05:13','tango/admin/aff41398c13a','polled_attr',5,'stoppedservers'),
-       (4,'2023-06-29 09:05:13','tango/admin/aff41398c13a','polled_attr',6,'1000'),
-       (4,'2023-06-29 09:05:13','tango/admin/aff41398c13a','polled_attr',7,'servers'),
-       (4,'2023-06-29 09:05:13','tango/admin/aff41398c13a','polled_attr',8,'1000'),
-       (4,'2023-06-29 09:05:13','tango/admin/aff41398c13a','polled_attr',9,'state'),
-       (4,'2023-06-29 09:05:13','tango/admin/aff41398c13a','polled_attr',10,'1000'),
-       (4,'2023-06-29 15:57:08','controller/basicdummymotorcontroller/dummy_ctrl','type',1,'Motor'),
-       (5,'2023-06-29 15:57:08','controller/basicdummymotorcontroller/dummy_ctrl','library',1,'DummyMotorController.py'),
-       (6,'2023-06-29 15:57:08','controller/basicdummymotorcontroller/dummy_ctrl','klass',1,'BasicDummyMotorController'),
-       (7,'2023-06-29 15:57:08','controller/basicdummymotorcontroller/dummy_ctrl','id',1,'1'),
-       (8,'2023-06-29 15:57:22','motor/dummy_ctrl/1','id',1,'2'),
-       (9,'2023-06-29 15:57:22','motor/dummy_ctrl/1','ctrl_id',1,'1'),
-       (10,'2023-06-29 15:57:22','motor/dummy_ctrl/1','axis',1,'1');

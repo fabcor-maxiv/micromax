518c98bd63f42b572de46d6ef53e9686b649f79c
Elmir Jagudin <elmir.jagudin@maxiv.lu.se>
2023-08-16 15:30:44 +0200

    ISARA emulator: deal with strings inside the emulator
    Convert the incoming messages to strings and convert
    replies to bytes when we are writing to the socket.
    
    Perform all the login of parsng and generting the replies
    as strings.

--- b-micromax-isara-0/emu.py
+++ b-micromax-isara-0/emu.py
@@ -5,13 +5,14 @@ from asyncio import StreamReader, StreamWriter
 from argparse import ArgumentParser
 from atcpserv import AsyncTCPServer
 
-ON = b"on"
-OFF = b"off"
-STATE = b"state"
-DI = b"di"
-DO = b"do"
-POSITION = b"position"
-MESSAGE = b"message"
+DI = "di"
+DO = "do"
+ON = "on"
+OFF = "off"
+PUT = "put"
+STATE = "state"
+POSITION = "position"
+MESSAGE = "message"
 
 
 def log(msg: str):
@@ -19,16 +20,16 @@ def log(msg: str):
     sys.stdout.flush()
 
 
-async def _read_command(connection_name: str, reader: StreamReader) -> bytes:
+async def _read_command(connection_name: str, reader: StreamReader) -> str:
     cmd = await reader.readuntil(b"\r")
     log(f"{connection_name}> {cmd}")
 
-    # chop off trailing \r
-    return cmd[:-1]
+    # chop off trailing \r and make a string
+    return cmd[:-1].decode()
 
 
-async def _write_reply(connection_name: str, writer: StreamWriter, reply: bytes):
-    reply += b"\r"
+async def _write_reply(connection_name: str, writer: StreamWriter, reply: str):
+    reply = (reply + "\r").encode()
     writer.write(reply)
     await writer.drain()
 
@@ -61,18 +62,18 @@ class _IsaraMixin:
         # needs model specific implementation
         raise NotImplementedError()
 
-    def _handle_on_command(self):
+    def _handle_on_command(self) -> str:
         self._power_on = True
-        return b"on"
+        return "on"
 
-    def _handle_off_command(self):
+    def _handle_off_command(self) -> str:
         self._power_on = False
-        return b"off"
+        return "off"
 
-    def _handle_message_command(self):
-        return b"System OK for operation"
+    def _handle_message_command(self) -> str:
+        return "System OK for operation"
 
-    def _handle_operate_command(self, command: bytes):
+    def _handle_operate_command(self, command: str) -> str:
         if command == ON:
             return self._handle_on_command()
         if command == OFF:
@@ -80,7 +81,7 @@ class _IsaraMixin:
 
         assert False, f"unexpected command {command} on operate connection"
 
-    def _handle_monitor_command(self, command: bytes) -> bytes:
+    def _handle_monitor_command(self, command: str) -> str:
         if command == STATE:
             return self._handle_state_command()
         if command == DI:
@@ -120,22 +121,19 @@ class Isara(_IsaraMixin):
     emulates ISARA robot, the first model, aka the blue robot at BioMAX
     """
 
-    def _handle_state_command(self) -> bytes:
-        reply = f"state({_encode(self._power_on)},1,0,,,,,-1,-1,,,,0,0,,75.0,1.16,1.17,1.18,,,,)"
-        return reply.encode()
+    def _handle_state_command(self) -> str:
+        return f"state({_encode(self._power_on)},1,0,,,,,-1,-1,,,,0,0,,75.0,1.16,1.17,1.18,,,,)"
 
-    def _handle_di_command(self):
-        reply = "di(" + "0" * 99 + ")"
-        return reply.encode()
+    def _handle_di_command(self) -> str:
+        return "di(" + "0" * 99 + ")"
 
-    def _handle_do_command(self):
-        reply = "do(" + "0" * 99 + ")"
-        return reply.encode()
+    def _handle_do_command(self) -> str:
+        return "do(" + "0" * 99 + ")"
 
-    def _handle_position_command(self) -> bytes:
-        return b"position(0.1,0.2,0.3,0.4,0.5,0.6)"
+    def _handle_position_command(self) -> str:
+        return "position(0.1,0.2,0.3,0.4,0.5,0.6)"
 
-    def _handle_monitor_command(self, command: bytes) -> bytes:
+    def _handle_monitor_command(self, command: str) -> str:
         # handle ISARA1 specific monitor commands
         if command == POSITION:
             return self._handle_position_command()
@@ -148,28 +146,27 @@ class Isara2(_IsaraMixin):
     emulates ISARA2 robot, aka the yellow robot at MicroMAX
     """
 
-    def _handle_state_command(self) -> bytes:
-        reply = (
+    def _handle_state_command(self) -> str:
+        return (
             f"state({_encode(self._power_on)},0,1,DoubleGripper,HOME,,1,1,-1,-1,-1,-1,-1,"
             "-1,-1,-1,,0,0,75.0,0,0,0.3865678,75.0,72.0,1,0,0,"
             "Robot is out of goniometer zone (translation),67108864,152.9,-390.8,"
             "-17.3,-180.0,0.0,89.1,-75.6,-18.8,93.6,0.0,105.3,-165.5,,1,,1,0,0,0,0,"
             "0,0,0,0,0,0,0,0,0,changetool|3|3|0|-2.441|0.068|392.37|0.0|0.0|-0.984)"
         )
-        return reply.encode()
 
-    def _handle_di_command(self) -> bytes:
+    def _handle_di_command(self) -> str:
         return (
-            b"di(0,0,0,0,0,0,0,1,1,1,1,1,1,0,0,0,1,0,1,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,"
-            b"0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,"
-            b"0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0)"
+            "di(0,0,0,0,0,0,0,1,1,1,1,1,1,0,0,0,1,0,1,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,"
+            "0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,"
+            "0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0)"
         )
 
-    def _handle_do_command(self) -> bytes:
+    def _handle_do_command(self) -> str:
         return (
-            b"do(0,0,1,0,0,1,0,1,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0"
-            b",0,0,0,0,0,1,0,0,0,0,0,0,1,0,0,0,0,0,1,1,0,0,0,0,0,1,1,0,0,1,1,0,0,0,0,0,0,0,0,"
-            b"0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0)"
+            "do(0,0,1,0,0,1,0,1,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0"
+            ",0,0,0,0,0,1,0,0,0,0,0,0,1,0,0,0,0,0,1,1,0,0,0,0,0,1,1,0,0,1,1,0,0,0,0,0,0,0,0,"
+            "0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0)"
         )
 
 

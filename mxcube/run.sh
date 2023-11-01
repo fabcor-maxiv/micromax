205e79835e12fce339219bc684e0d67664f9e0c2
Elmir Jagudin <elmir.jagudin@maxiv.lu.se>
2023-11-01 17:17:13 +0100

    mxcube: update mxcube start script name
    The old 'mxcube3-server' script for starting the back-end is
    now renamed to 'mxcubeweb-server'.

--- mxcube/run.sh
+++ mxcube/run.sh
@@ -2,4 +2,4 @@
 
 # stop circus managed mxcube and run new mxcube in this terminal
 circusctl stop mxcube
-/app/web/mxcube3-server --repository /app/conf/MicroMAX --static-folder /app/web/ui/build
+/app/web/mxcubeweb-server --repository /app/conf/MicroMAX --static-folder /opt/mxcube/build

11529e50ab3b8b3f274f63efb40fc80077d464f3
Elmir Jagudin <elmir.jagudin@maxiv.lu.se>
2023-11-21 12:23:07 +0100

    shutter: emulate open and close operations

--- micromax-ds/beam_shutter.py
+++ micromax-ds/beam_shutter.py
@@ -1,12 +1,24 @@
 #!/usr/bin/env python
 
-from tango.server import Device, attribute
+from tango.server import Device, attribute, command
 
 
 class BeamShutter(Device):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self._closed = False
+
     @attribute(name="StatusClosed", dtype=bool)
     def status_closed(self):
-        return False
+        return self._closed
+
+    @command
+    def Open(self):
+        self._closed = False
+
+    @command
+    def Close(self):
+        self._closed = True
 
 
 if __name__ == "__main__":

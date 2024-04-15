9c2e0cc53ef294138c0a3269ea6a368f6f1bc654
Elmir Jagudin <elmir.jagudin@maxiv.lu.se>
2024-04-15 17:11:57 +0200

    MD3 Video: vary corner color in frames
    Color upper left corner of each frame in varying colors.
    This way it is noticeable if video stream dies.

--- b-micromax-md3-pc/md3video.py
+++ b-micromax-md3-pc/md3video.py
@@ -1,10 +1,13 @@
 #!/usr/bin/env python
-from sys import stdout
+import time
+import numpy
 import struct
-import random
 from pathlib import Path
+from random import random
 from tango.server import Device, attribute
 from simplejpeg import decode_jpeg
+
+CORNER_SIZE = 8
 
 # monochrome, 8-bit per pixel
 IMAGE_MODE_L = 0
@@ -20,33 +23,51 @@ HEADER_SIZE = 32
 NUM_PIXELS = WIDTH * HEIGHT
 
 
-def _load_image() -> bytearray:
-    jpg_file = Path(SAMPLE_JPEG)
-    pixels = decode_jpeg(jpg_file.read_bytes())
+def _paint_corner(orig, color):
+    pixels = numpy.copy(orig)
+    for i in range(CORNER_SIZE):
+        pixels[i][0:CORNER_SIZE] = color
 
-    header = struct.pack(
-        ">IHHqiiHHxxxx", 1447314767, 1, IMAGE_MODE, 0, WIDTH, HEIGHT, 0, HEADER_SIZE
+    return bytearray(pixels)
+
+
+def _make_image(header, pixels, corner_color):
+    return header + _paint_corner(pixels, corner_color)
+
+
+def _load_images():
+    sample_pixels = decode_jpeg(Path(SAMPLE_JPEG).read_bytes())
+
+    header = bytearray(
+        struct.pack(
+            ">IHHqiiHHxxxx", 1447314767, 1, IMAGE_MODE, 0, WIDTH, HEIGHT, 0, HEADER_SIZE
+        )
     )
 
-    return bytearray(header) + bytearray(pixels)
+    yield header + bytearray(sample_pixels)
+    yield _make_image(header, sample_pixels, [0, 0, 0])
+    yield _make_image(header, sample_pixels, [255, 0, 0])
+    yield _make_image(header, sample_pixels, [0, 255, 0])
+    yield _make_image(header, sample_pixels, [0, 0, 255])
 
 
 class MD3(Device):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self._image = _load_image()
-        self._frame_number = 1
+        self._images = list(_load_images())
+        self._frame_number = 0
 
-    def _update_image(self):
-        # update frame number in the header
-        self._image[8:16] = struct.pack(">q", self._frame_number)
+    def _get_image(self):
         self._frame_number += 1
+        image = self._images[self._frame_number % len(self._images)]
+        # update frame number in the header
+        image[8:16] = struct.pack(">q", self._frame_number)
+
+        return image
 
     @attribute(dtype="DevEncoded", format="%d")
     def video_last_image(self):
-        self._update_image()
-
-        return "VIDEO_IMAGE", self._image
+        return "VIDEO_IMAGE", self._get_image()
 
     @attribute(dtype="DevULong")
     def image_width(self):

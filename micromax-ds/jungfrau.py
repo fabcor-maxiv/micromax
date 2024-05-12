a600a7030524ccd763e68befe6265471da623f32
Elmir Jagudin <elmir.jagudin@maxiv.lu.se>
2024-05-12 14:37:40 +0200

    Jungfrau: more detailed emulation
    * emulate tango device state closer to the real device:
      armed & aquiring -> RUNNING
      idle             -> ON
    * add SoftwareTrigger() command
    * add storage_cell_count attribute

--- micromax-ds/jungfrau.py
+++ micromax-ds/jungfrau.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 import time
+from enum import Enum
 from typing import Optional, Union, Any
 from dataclasses import dataclass
 from threading import Thread
@@ -9,6 +10,7 @@ from tango.server import Device, attribute, command
 
 ARM_DELAY = 1.2
 DISARM_DELAY = 0.8
+SOFTWARE_TRIGGERED_COLLECTION_LENGTH = 12.0
 
 
 @dataclass
@@ -23,6 +25,7 @@ class _WriteableAttr:
 WRITEABLE_ATTRIBUTES = [
     _WriteableAttr("beam_x_pxl", float, 0.0),
     _WriteableAttr("beam_y_pxl", float, 0.0),
+    _WriteableAttr("storage_cell_count", int, 15),
     _WriteableAttr("file_prefix", str, ""),
     _WriteableAttr("photon_energy_keV", float, 12.0),
     _WriteableAttr("detector_distance_mm", float, 0),
@@ -38,6 +41,17 @@ WRITEABLE_ATTRIBUTES = [
 ]
 
 
+class _DetectorState(Enum):
+    IDLE = 1  # unarmed
+    MEASURING = 2  # armed and possibly taking pictures
+
+
+DETECTOR_TO_TANGO_STATE = {
+    _DetectorState.IDLE: DevState.ON,
+    _DetectorState.MEASURING: DevState.RUNNING,
+}
+
+
 def delayed(delay, delayed_call):
     def run():
         time.sleep(delay)
@@ -50,7 +64,7 @@ class Jungfrau(Device):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
-        self._armed = False
+        self._state = _DetectorState.IDLE
 
         self._attribute_vals = dict()
         self._init_writeable_attributes()
@@ -89,27 +103,34 @@ class Jungfrau(Device):
             self._attribute_vals[attr.name] = attr.default_value
 
     def dev_state(self) -> DevState:
-        if self._armed:
-            return DevState.RUNNING
-
-        return DevState.ON
+        return DETECTOR_TO_TANGO_STATE[self._state]
 
     @command
     def Arm(self):
         # emulate that it take a second or so to arm the detector
         time.sleep(ARM_DELAY)
-        self._armed = True
+        self._state = _DetectorState.MEASURING
+
+    @command
+    def SoftwareTrigger(self):
+        if self._state != _DetectorState.MEASURING:
+            raise Exception("detector needs to be in armed state")
+
+        def datacollection_finished():
+            self._state = _DetectorState.IDLE
+
+        delayed(SOFTWARE_TRIGGERED_COLLECTION_LENGTH, datacollection_finished)
 
     @command
     def Stop(self):
         def set_armed_false():
-            self._armed = False
+            self._state = _DetectorState.IDLE
 
-        if not self._armed:
-            # if not armed, this is a NOP command
+        if self._state != _DetectorState.MEASURING:
+            # if not armed nor measuring, this is a NOP command
             return
 
-        # emulate that it takes some time to dis-arm the detector
+        # emulate that it takes some time to dis-arm and stop the detector
         delayed(DISARM_DELAY, set_armed_false)
 
 
